8f9fd1e2f9a4cc46d9b6f2a988342f73b39fb933
João Vítor <joao.vitor.paes@hotmail.com>
2026-05-19 14:48:45 +0200

    Replace axios with fetch for API requests
    Refactored service to use fetch API instead of axios.

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,7 +1,35 @@
-import axios from "axios";
-
 const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3001/";
 
-const service = axios.create({ baseURL });
+const request = async (url, options = {}) => {
+  const response = await fetch(`${baseURL}${url}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Erro HTTP: ${response.status}`);
+  }
+  
+  const text = await response.text();
+  return text ? JSON.parse(text) : null;
+};
+
+const service = {
+  get: (url, options = {}) =>
+    request(url, {
+      method: "GET",
+      ...options,
+    }),
+
+  patch: (url, data, options = {}) =>
+    request(url, {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      ...options,
+    }),
+};
 
 export default service;
